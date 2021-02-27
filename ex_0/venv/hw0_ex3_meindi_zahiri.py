9ff8510275697fe22b01d142dbe950fcde444a8f
dekapon <lemeindi@gmail.com>
2021-02-27 15:00:26 +0100

    ex 3 cleaned

--- ex_0/venv/hw0_ex3_meindi_zahiri.py
+++ ex_0/venv/hw0_ex3_meindi_zahiri.py
@@ -3,8 +3,6 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import PIL.Image
 
-# Read Images
-# img = mpimg.imread('stopsign.jpg') plt
 img = PIL.Image.open("stopsign.jpg") # PIL
 
 size = img.size
@@ -12,20 +10,12 @@ size = img.size
 # 83 6 5
 tMin = 70
 tMax = 110
-displayed = 0
 for x in range(size[0]):
     for y in range(size[1]):
         if img.getpixel((x, y))[0] < tMin or img.getpixel((x, y))[0] > tMax or img.getpixel((x, y))[1] > 20 or img.getpixel((x, y))[2] > 10:
             img.putpixel((x, y), (0, 0, 0))
         else:
             img.putpixel((x, y), (255, 0, 0))
-    percentage = 100*x/size[0]
-    if displayed == 0 and percentage % 5 == 0:
-        print("{:.0f}".format(percentage))
-        displayed = 1
-    displayed = 0
-
-
 plt.imshow(img)
 plt.show()
 img.save('result.jpg')
