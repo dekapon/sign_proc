f82226ea05ffc57c3ad8cde0da8b3133cbb707ec
dekapon <lemeindi@gmail.com>
2021-03-14 12:33:28 +0100

    1.3 done

--- ex_1/hw1_ex2_meindi_zahiri.py
+++ ex_1/hw1_ex2_meindi_zahiri.py
@@ -1,8 +1,12 @@
 from scipy.io import wavfile
+import soundfile as sf
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 print(f'Hello world homework 1 ex 1')
+test = 1 # put to 0 before sending assignment
+t = time.time()
 
 # 1.1
 def uniform_quantization(inputSig, min_amplitude, max_amplitude, quantization_levels):
@@ -20,6 +24,7 @@ def uniform_quantization(inputSig, min_amplitude, max_amplitude, quantization_le
             outputSig[i] = best_match
     return outputSig
 
+
 # 1.2
 f = 0.2  # value in Hz (default is 1)
 duration = 5  # s
@@ -32,16 +37,11 @@ max_amplitude = 1
 
 n = x.size
 m = 3
-quantSin = [[0] * m for i in range(m)]
+quantSin = [[0] * m for i in range(n)]
 plt.plot(x, y)
+quantization_levels = [2, 4, 16]
 for i in range(m):
-    if i == 0:
-        quantization_levels = 2
-    elif i == 1:
-        quantization_levels = 4
-    elif i == 2:
-        quantization_levels = 16
-    quantSin[i] = uniform_quantization(y, min_amplitude, max_amplitude, quantization_levels)
+    quantSin[i] = uniform_quantization(y, min_amplitude, max_amplitude, quantization_levels[i])
     plt.plot(x, quantSin[i])
 
     # plt.ax.plot([1, 2, 3])
@@ -50,3 +50,54 @@ plt.legend(["Sine", "2 levels quant.", "4 levels quant.", "16 levels quant."])
 plt.show()
 
 # 1.3
+# samplerate, audioIn = wavfile.read('peter.wav')
+audioIn, samplerate = sf.read('peter.wav')
+
+if test:
+    bitsDepth = [2, 3, 4]
+else:
+    """
+    It doesnt work with 16, it takes too long to compute, but i checked the audio file 
+    and it seems to be already 16 bits depth.
+    
+    So i put 8 bits depth instead of 16, execution time goes from 2.5s to 16s.
+    
+    When i profile my code, i see that the program is spending 96% of the time in 
+    my uniform quantization function, because i iterate through a large array 
+    for the comparison with each sample.
+    """
+    bitsDepth = [2, 4, 8]
+
+quantization_levels = len(bitsDepth) * [0]
+
+timeInterval = [0.5, 0.51]
+samplePos = [samplerate * timeInterval[0], samplerate * timeInterval[1]]
+# print(samplePos)
+samplePos = [int(item) for item in samplePos]  # items in the list are float, so i convert them to int
+
+
+plt.figure()
+x = np.linspace(0, audioIn.size, audioIn.size)
+plt.plot(x[samplePos[0]:samplePos[1]], audioIn[samplePos[0]:samplePos[1]])
+m = len(bitsDepth)
+quantAudio = [[0] * m for i in range(n)]
+
+min_amplitude = min(audioIn[samplePos[0]:samplePos[1]])
+max_amplitude = max(audioIn[samplePos[0]:samplePos[1]])
+# print(str(min_amplitude) + "   " + str(max_amplitude))
+for i in range(len(bitsDepth)):
+    quantization_levels[i] = 2 ** bitsDepth[i]
+    quantAudio[i] = uniform_quantization(audioIn, min_amplitude, max_amplitude, quantization_levels[i])
+    plt.plot(x[samplePos[0]:samplePos[1]], quantAudio[i][samplePos[0]:samplePos[1]])
+    # wavfile.write("peter_" + str(bitsDepth[i]) + " bits depth.wav",samplerate,quantAudio[i])
+    sf.write("peter_" + str(bitsDepth[i]) + " bits depth.wav",quantAudio[i],samplerate)
+plt.legend(["original audio", "2 bits depth quant.", "4 bits depth quant.", "8 bits depth quant."])
+plt.show()
+# print(quantAudio[1])
+# sf.write('new_file.flac', data, samplerate)
+
+elapsed = time.time() - t
+print("done, elapsed time: " + str(elapsed) + " s")
+
+
+
