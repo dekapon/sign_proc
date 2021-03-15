6583b323ab0da9b4dda8bfe381e734e6bbeafb37
dekapon <lemeindi@gmail.com>
2021-03-15 14:58:38 +0100

    ex 3 started

---
+++ ex_1/hw1_ex3_meindi_zahiri.py
@@ -0,0 +1,18 @@
+from scipy.io import wavfile
+import soundfile as sf
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+print(f'Hello world homework 1 ex 3')
+
+def lin_int(y_vals, x):
+
+
+
+
+elapsed = time.time() - t
+print("done, elapsed time: " + str(elapsed) + " s")
+
+
+
